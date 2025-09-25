8a2ec73564bfe9c47394cf8d64cff0ec18565d87
sallu-50 <salmanabdullahal0@gmail.com>
2025-09-25 18:23:06 +0600

    update search form

--- resources/views/components/search-bar.blade.php
+++ resources/views/components/search-bar.blade.php
@@ -1,4 +1,7 @@
-<form action="{{ route('search.index') }}" method="GET" class="flex items-center">
-    <input type="text" name="query" placeholder="Search for products..." class="border rounded-l-md p-2 w-64" required value="{{ request('query') }}">
-    <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded-r-md hover:bg-blue-700">Search</button>
+<form action="{{ route('search.index') }}" method="GET" class="flex items-center w-full md:w-auto">
+    <input type="text" name="query" placeholder="Search for products..." class="border rounded-l-md p-2 w-full md:w-64"
+        required value="{{ request('query') }}">
+    <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded-r-md hover:bg-blue-700">
+        Search
+    </button>
 </form>
