153e7f381b27da21b1d9d35ade4eff019b893135
sallu-50 <salmanabdullahal0@gmail.com>
2025-09-25 18:34:53 +0600

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,8 @@
     <script src="//unpkg.com/alpinejs" defer></script>
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 </head>
 
 <body class="bg-gray-100 text-gray-800">
